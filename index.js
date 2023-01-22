cac4bdfca89bbf53740c9ea69a881afa37518f58
Laura Brea <laurabrea17@gmail.com>
2023-01-22 13:22:03 -0500

    added a function but only one card flips,need to find a solution for all to flip

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-const memoryCard= document.getElementById("memoryCard")
-memoryCard.addEventListener("click", flipCard);
+const cards=document.getElementById("memoryCard");
+cards.addEventListener("click", flipCard);
 
 function flipCard() {
-    memoryCard.classList.toggle("flipCard");
+    cards.classList.toggle("flipCard");
 }
+
+
+//const cards=document.querySelectorAll(".memoryCard");
+//function flipCard(){
+   // cards.forEach((card)=>{
+     //   if (card != this){
+     //       card.classList.remove('is-flipped');
+       // }
+   // });
+   // this.classList.toggle('is-flipped');
+
+//};
+//cards.forEach((card)=>card.addEventListener("click", flipCard));
+
+
+//function flipCard() {
+  //  cards.forEach((card) =>{
+    //    if(card != this){
+      //      card.classList.remove('is-flipped');
+        //}
+    //});
+    //this.classList.toggle('is-flipped');
+//};
+//cards.forEach((card)=> card.addEventListener("click", flipCard));
