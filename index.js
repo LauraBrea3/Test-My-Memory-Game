137930d9aed4faa0c0ee79f4c7cb7f8066a0b563
Laura Brea <laurabrea17@gmail.com>
2023-01-24 17:40:33 -0500

    testing out functions for matching cards

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const cards = document.getElementsByClassName('memoryCard'); 
+let isFlipped = false;
+let firstCard;
+let secondCard;
+
 
 for (let i = 0; i < cards.length; i++) { 
-cards[i].addEventListener("click", flipCard); 
-} 
+cards[i].addEventListener('click', flipCard); 
+}
 
 function flipCard() { 
-this.classList.toggle("flipCard"); }
+this.classList.toggle('flipCard'); 
 
+if(!isFlipped){
+  isFlipped = true;
+  firstCard = this;
+  return;
+}
+  secondCard = this;
+  isFlipped = false;
+
+  cardMatch();
+}
+
+function cardMatch(){
+  if(firstCard.dataset.imageType === secondCard.dataset.imageType){
+    disableCard();
+    return;
+  }
+unflipCard();
+}
+
+function disableCard(){
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click',flipCard);
+}
+
+function unflipCard(){
+  setTimeout(() =>{
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
+}
 
 /*const cards=document.querySelectorAll('.memoryCard');
 let isFlipped = false;
